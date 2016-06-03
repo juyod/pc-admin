262c5607e45a30b8e5e38ba859cb1eae61c4e612
gagelee.li <gagelee.li@zkteco.com>
2016-06-03 16:24:22 +0800

    remove some html

--- src/app/app.js
+++ src/app/app.js
@@ -1,17 +1,18 @@
-'use strict';
-
-angular.module('PCAdmin', [
-  'ngAnimate',
-  'ui.bootstrap',
-  'ui.sortable',
-  'ui.router',
-  'ngTouch',
-  'toastr',
-  'smart-table',
-  "xeditable",
-  'ui.slimscroll',
-  'ngJsTree',
-  'angular-progress-button-styles',
-  'PCAdmin.theme',
-  'PCAdmin.pages'
-]);
+(function() {
+  'use strict';
+  angular.module('PCAdmin', [
+    'ngAnimate',
+    'ui.bootstrap',
+    'ui.sortable',
+    'ui.router',
+    'ngTouch',
+    'toastr',
+    'smart-table',
+    "xeditable",
+    'ui.slimscroll',
+    'ngJsTree',
+    'angular-progress-button-styles',
+    'PCAdmin.theme',
+    'PCAdmin.pages'
+  ]);
+})();
