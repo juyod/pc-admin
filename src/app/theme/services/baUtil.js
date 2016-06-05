0064aacc78d5548532baa9641af363ff5f064a8d
juyod <deisvn@gmail.com>
2016-06-05 22:38:30 +0800

    update

--- src/app/theme/services/baUtil.js
+++ src/app/theme/services/baUtil.js
@@ -2,30 +2,28 @@
  * @author v.lugovsky
  * created on 03.05.2016
  */
-(function () {
+(function() {
   'use strict';
 
   angular.module('PCAdmin.theme')
-      .service('baUtil', baUtil);
+    .service('baUtil', baUtil);
 
   /** @ngInject */
   function baUtil() {
-
     this.isDescendant = function(parent, child) {
       var node = child.parentNode;
-      while (node != null) {
-        if (node == parent) {
+      while (node) {
+        if (node === parent) {
           return true;
         }
         node = node.parentNode;
       }
       return false;
     };
-
     this.hexToRGB = function(hex, alpha) {
-      var r = parseInt( hex.slice(1,3), 16 );
-      var g = parseInt( hex.slice(3,5), 16 );
-      var b = parseInt( hex.slice(5,7), 16 );
+      var r = parseInt(hex.slice(1, 3), 16);
+      var g = parseInt(hex.slice(3, 5), 16);
+      var b = parseInt(hex.slice(5, 7), 16);
       return 'rgba(' + r + ', ' + g + ', ' + b + ', ' + alpha + ')';
     }
   }
