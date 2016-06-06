508655831d19340b848067ec3720ea100c42642b
juyod <deisvn@gmail.com>
2016-06-07 06:10:23 +0800

    update

--- src/app/pages/dashboard/dashboardTopMsg/dashboardTopMsg.ctrl.js
+++ src/app/pages/dashboard/dashboardTopMsg/dashboardTopMsg.ctrl.js
@@ -15,8 +15,20 @@
     }, function() {
       $scope.curOnline = '暂无数据';
     });
-    DashboardTopMsgService.loadHistoryOnline().then(function() {}, function() {});
-    DashboardTopMsgService.loadPeopleFlow().then(function() {}, function() {});
-    DashboardTopMsgService.loadLatestAnnouncement().then(function() {}, function() {});
+    DashboardTopMsgService.loadHistoryOnline().then(function(data) {
+      $scope.historyOnline = data;
+    }, function() {
+      $scope.historyOnline = '暂无数据';
+    });
+    DashboardTopMsgService.loadPeopleFlow().then(function(data) {
+      $scope.peopleFlow = data;
+    }, function() {
+      $scope.peopleFlow = '暂无数据';
+    });
+    DashboardTopMsgService.loadLatestAnnouncement().then(function(data) {
+      $scope.latestAnnouncement = data;
+    }, function() {
+      $scope.latestAnnouncement = '暂无数据';
+    });
   }
 })();
